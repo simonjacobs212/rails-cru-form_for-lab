3bb9dda37875cbe9a011b47fc8a300fe3b10849e
Simon Jacobs <simonadlerjacobs@gmail.com>
2021-01-14 16:14:11 -0500

    commit tree e2d6c776109f9738ee009e39fd63d8366fcc920f

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,36 @@
+class GenresController < ApplicationController
+  before_action :set_genre, only: [:show, :edit, :update]
+
+  def index
+    @genres = Genre.all
+  end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.new(genre_params(:name))
+    @genre.save
+    redirect_to genre_path(@genre)
+  end
+
+  def show
+  end
+
+  def update
+    @genre.update(genre_params(:name))
+    redirect_to genre_path(@genre)
+  end
+
+  private
+
+  def set_genre
+    @genre = Genre.find(params[:id])
+  end
+
+  def genre_params(*args)
+    params.require(:genre).permit(*args)
+  end
+
+end
